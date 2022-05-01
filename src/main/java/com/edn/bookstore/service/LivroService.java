669c2a60eed8e6dcc0d3f4b1ff7bd2f88e0af8ac
ericksonneres <erickson.neres@gmail.com>
2022-05-01 13:49:17 -0300

    add livro dto

---
+++ src/main/java/com/edn/bookstore/service/LivroService.java
@@ -0,0 +1,33 @@
+package com.edn.bookstore.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edn.bookstore.domain.Livro;
+import com.edn.bookstore.repositories.LivroRepository;
+import com.edn.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+	
+	@Autowired
+	private LivroRepository repository;
+	
+	@Autowired
+	private CategoriaService categoriaService; 
+	
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Object não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return  repository.findAllByCategoria(id_cat);
+	}
+
+}
