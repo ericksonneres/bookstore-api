c9d41d845f02edd15a1e80a05af6984e22e41414
ericksonneres <erickson.neres@gmail.com>
2022-07-19 22:55:15 -0300

    post e put para livro

--- src/main/java/com/edn/bookstore/service/LivroService.java
+++ src/main/java/com/edn/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.edn.bookstore.domain.Categoria;
 import com.edn.bookstore.domain.Livro;
 import com.edn.bookstore.repositories.LivroRepository;
 import com.edn.bookstore.service.exceptions.ObjectNotFoundException;
@@ -28,6 +29,25 @@ public class LivroService {
 	public List<Livro> findAll(Integer id_cat) {
 		categoriaService.findById(id_cat);
 		return  repository.findAllByCategoria(id_cat);
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateDate(newObj, obj);
+		return repository.save(obj);
+	}
+
+	private void updateDate(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());		
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 
 }
