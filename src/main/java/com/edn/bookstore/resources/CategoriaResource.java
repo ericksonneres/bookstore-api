3e69171e1a87fc84d6c294085d3dba974d69764f
ericksonneres <erickson.neres@gmail.com>
2022-07-26 20:57:56 -0300

    ajuste em deletemapping

--- src/main/java/com/edn/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/edn/bookstore/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,7 +60,7 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
 	}
 	
-	@GetMapping(value = "/{id}")
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
